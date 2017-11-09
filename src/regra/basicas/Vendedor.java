37f5fb52f8667789a7ecfae94683422d74bbe5e9
Gildo Neto <gildorama@gmail.com>
2017-11-09 20:24:02 -0300

    Alteração da classe básica Vendedor

--- src/regra/basicas/Vendedor.java
+++ src/regra/basicas/Vendedor.java
@@ -7,8 +7,37 @@ package regra.basicas;
 
 /**
  *
- * @author paulo
+ * @author gildo
  */
 public class Vendedor {
-    
+
+    /**
+     * @return the vend_cod
+     */
+    public Integer getVend_cod() {
+        return vend_cod;
+    }
+
+    /**
+     * @param vend_cod the vend_cod to set
+     */
+    public void setVend_cod(Integer vend_cod) {
+        this.vend_cod = vend_cod;
+    }
+
+    /**
+     * @return the vend_nome
+     */
+    public String getVend_nome() {
+        return vend_nome;
+    }
+
+    /**
+     * @param vend_nome the vend_nome to set
+     */
+    public void setVend_nome(String vend_nome) {
+        this.vend_nome = vend_nome;
+    }
+    private Integer vend_cod;
+    private String vend_nome;
 }
