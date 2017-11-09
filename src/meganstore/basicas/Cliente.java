679fbaeb45fd5399a1f16cea8686e362bbe1eef7
William <>
2017-11-09 19:39:22 -0300

    Criação Da Classe Básica Cliente

---
+++ src/meganstore/basicas/Cliente.java
@@ -0,0 +1,36 @@
+  
+package meganstore.basicas;
+
+
+public class Cliente {
+
+    private Integer cli_cpf;
+    private String cli_tel;
+    private String cli_nome;
+
+    public Integer getCli_cpf() {
+        return cli_cpf;
+    }
+
+    public void setCli_cpf(Integer cpf) {
+        this.cli_cpf = cpf;
+    }
+
+    public String getCli_tel() {
+        return cli_tel;
+    }
+
+    public void setCli_tel(String tel) {
+        this.cli_tel = tel;
+    }
+
+    public String getCli_nome() {
+        return cli_nome;
+    }
+
+    public void setCli_nome(String nome) {
+        this.cli_nome = nome;
+    }
+
+}    
+
