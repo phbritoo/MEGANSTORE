f5c1139ae247dc0284a89587dbc5f35e0130e94d
Paulo <paulohbsantana11@gmail.com>
2017-11-09 19:49:41 -0300

    classses basicas

---
+++ src/meganstore/basicas/Vendedor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meganstore.basicas;
+
+/**
+ *
+ * @author paulo
+ */
+public class Vendedor {
+    
+}
